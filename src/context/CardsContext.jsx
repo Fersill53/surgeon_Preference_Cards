dd0dadf0ddc59d5c261b7f6206b65e4c69b7e262
Fernando Siller <fernando@Fernandos-MBP.home.local>
2025-08-27 21:21:28 -0500

    added comments for what code will look like

---
+++ src/context/CardsContext.jsx
@@ -0,0 +1,47 @@
+/*
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+import { loadCards, saveCards } from '../utils/storage'
+import { nanoid } from 'nanoid'
+
+cosnt CardsContext = createContext(null)
+
+export function CardsProvider({ children }) {
+    const [cards, setCards] = useState(() => loadCards())
+
+    useEffect(() => { saveCards(cards) }, [cards])
+
+// cards to align with hospital style
+
+const createCard = (data) => {
+    const card = {
+    id: nanoid()
+    surgeonName: '',
+    procedureName: '',
+    gloveSize: '',
+    instruments: [],
+    sutures: [],
+    meds: [],
+    notes: '',
+    ...data
+        }
+    setCards(prev => [card, ...prev])
+    return card.id
+    }
+
+    const updateCard = (id, patch) => {
+        setCards(prev => prev.map(c => c.id === id ? {...c, ...patch } : c))
+        }
+
+    const deleteCard = (id) => {
+        setCards(prev => prev.filter(c => c.id !== id))
+        }
+
+    const value = useMemo(() => ({
+        cards, setCards, createCard, updateCard, deleteCard
+        }), [cards])
+
+    return <Cards.Context.Provider value={value}>{children}</CardsContext.Provider>
+
+}
+
+exprot const userCards = () => useCOntext(CardsContext)
