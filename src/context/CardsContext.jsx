d26c41c82337a875ced4f709f64c64ab92010b00
Fernando Siller <fernando@Fernandos-MBP.home.local>
2025-08-30 16:54:24 -0500

    typo left

--- src/context/CardsContext.jsx
+++ src/context/CardsContext.jsx
@@ -14,7 +14,7 @@ export function CardsProvider({ children }) {
 
 const createCard = (data) => {
     const card = {
-    id: nanoid()
+    id: nanoid(),
     surgeonName: '',
     procedureName: '',
     gloveSize: '',
